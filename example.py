ddbdc4a6c4a364cd8e834a4e8075eccc3bfeb0bd
НесмитС <timoschka12@gmail.com>
2025-05-14 12:52:40 +0300

    Initial commit

---
+++ example.py
@@ -0,0 +1,8 @@
+# ваше решение
+
+dist = input("Введите расстояние до работы в километрах: ")
+mil = 1.6
+some_value = int(dist)
+result = f"Расстояние до работы в милях: {some_value * mil}"
+print(result)
+
