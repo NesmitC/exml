1b673476a5539607aca18d04f9f56cb4adce3d78
НесмитС <timoschka12@gmail.com>
2025-05-14 13:18:39 +0300

    Обновил example.py - изменил мили значение

--- example.py
+++ example.py
@@ -1,7 +1,17 @@
+''' Быстрый чек-лист на каждый день
+        git status       # Проверить изменения
+        git pull        # Забрать обновления
+
+        # ... Работа ...
+
+        git add .       # Добавить изменения
+        git commit -m "Update"
+        git push        # Отправить на GitHub
+'''
 # ваше решение
 
 dist = input("Введите расстояние до работы в километрах: ")
-mil = 1.6
+mil = 1.8
 some_value = int(dist)
 result = f"Расстояние до работы в милях: {some_value * mil}"
 print(result)
